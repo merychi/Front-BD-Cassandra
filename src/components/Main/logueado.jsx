5c54d186abaf4e7aae150fe3f5608a37ba215877
merrychi <merryamchitty@gmail.com>
2025-01-17 11:57:04 -0400

    pantalla logueada lista exitosa

--- src/components/Main/logueado.jsx
+++ src/components/Main/logueado.jsx
@@ -1,59 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import StatusBar from "./statusbar";
+import axios from "axios";
 import "../../global.css";
 
 export const Logueado = () => {
   const [movies, setMovies] = useState([]);
   const [recommendedMovies, setRecommendedMovies] = useState([]);
   const [randomMovie, setRandomMovie] = useState(null);
+  const userID = localStorage.getItem("userID");
 
   useEffect(() => {
-    const mockMovies = [
-      { id: 1, title: "Interestellar", poster_path: "https://i.imgur.com/mClzJYp.png" },
-      { id: 2, title: "Her", poster_path: "https://i.imgur.com/6OD4OjT.png" },
-      { id: 3, title: "Joker", poster_path: "https://i.imgur.com/CUhrd1i.png" },
-      { id: 4, title: "Maze Runner", poster_path: "https://i.imgur.com/okOFya0.png" },
-      { id: 5, title: "Stranger Things", poster_path: "https://i.imgur.com/erTqXbm.png" },
-      { id: 6, title: "El Niño Cohete", poster_path: "https://i.imgur.com/SrUyIjt.png" },
-      { id: 7, title: "Tiempo", poster_path: "https://i.imgur.com/0FyogES.png" },
-      { id: 8, title: "Reyes del Futuro", poster_path: "https://i.imgur.com/OSLQdAR.png" },
-      { id: 9, title: "Avengers: Infinity War", poster_path: "https://i.imgur.com/3RMOOZ5.png" },
-      { id: 10, title: "La forma del agua", poster_path: "https://i.imgur.com/RQD14cu.png" },
-      { id: 11, title: "SHERK", poster_path: "https://i.imgur.com/9gkryX5.png" },
-      { id: 12, title: "Géminis", poster_path: "https://i.imgur.com/nLUe7Cg.png" },
-      { id: 13, title: "DJANJO", poster_path: "https://i.imgur.com/CZJW1Pz.png" },
-      { id: 14, title: "Interestellar", poster_path: "https://i.imgur.com/mClzJYp.png" },
-      { id: 92, title: "Her", poster_path: "https://i.imgur.com/6OD4OjT.png" },
-      { id: 23, title: "Joker", poster_path: "https://i.imgur.com/CUhrd1i.png" },
-      { id: 24, title: "Maze Runner", poster_path: "https://i.imgur.com/okOFya0.png" },
-      { id: 25, title: "Stranger Things", poster_path: "https://i.imgur.com/erTqXbm.png" },
-      { id: 62, title: "El Niño Cohete", poster_path: "https://i.imgur.com/SrUyIjt.png" },
-      { id: 27, title: "Tiempo", poster_path: "https://i.imgur.com/0FyogES.png" },
-      { id: 28, title: "Reyes del Futuro", poster_path: "https://i.imgur.com/OSLQdAR.png" },
-      { id: 29, title: "Avengers: Infinity War", poster_path: "https://i.imgur.com/3RMOOZ5.png" },
-      { id: 30, title: "La forma del agua", poster_path: "https://i.imgur.com/RQD14cu.png" },
-      { id: 31, title: "SHERK", poster_path: "https://i.imgur.com/9gkryX5.png" },
-      { id: 32, title: "Géminis", poster_path: "https://i.imgur.com/nLUe7Cg.png" },
-      { id: 33, title: "DJANJO", poster_path: "https://i.imgur.com/CZJW1Pz.png" },
-      { id: 41, title: "Interestellar", poster_path: "https://i.imgur.com/mClzJYp.png" },
-      { id: 42, title: "Her", poster_path: "https://i.imgur.com/6OD4OjT.png" },
-      { id: 43, title: "Joker", poster_path: "https://i.imgur.com/CUhrd1i.png" },
-      { id: 44, title: "Maze Runner", poster_path: "https://i.imgur.com/okOFya0.png" },
-      { id: 45, title: "Stranger Things", poster_path: "https://i.imgur.com/erTqXbm.png" },
-      { id: 46, title: "El Niño Cohete", poster_path: "https://i.imgur.com/SrUyIjt.png" },
-      { id: 47, title: "Tiempo", poster_path: "https://i.imgur.com/0FyogES.png" },
-      { id: 48, title: "Reyes del Futuro", poster_path: "https://i.imgur.com/OSLQdAR.png" },
-      { id: 49, title: "Avengers: Infinity War", poster_path: "https://i.imgur.com/3RMOOZ5.png" },
-      { id: 50, title: "La forma del agua", poster_path: "https://i.imgur.com/RQD14cu.png" },
-      { id: 51, title: "SHERK", poster_path: "https://i.imgur.com/9gkryX5.png" },
-      { id: 52, title: "Géminis", poster_path: "https://i.imgur.com/nLUe7Cg.png" },
-      { id: 53, title: "DJANJO", poster_path: "https://i.imgur.com/CZJW1Pz.png" },
-    ];
-    setMovies(mockMovies);
-    setRecommendedMovies(mockMovies.slice(0, 8));
-    setRandomMovie(mockMovies[0]);
-  }, []);
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/movies/'); 
+        if (Array.isArray(response.data.data)) {
+          setMovies(response.data.data);
+          setRandomMovie(response.data.data[0]); 
+        } else {
+          console.error("Los datos recibidos no están en el formato esperado.");
+          setMovies([]);
+        }
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+        setMovies([]);
+      }
+    };
+
+    const fetchRecommendations = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/recommendations/${userID}`);
+        setRecommendedMovies(response.data.data); 
+      } catch (error) {
+        console.error("Error fetching recommendations:", error);
+      }
+    };
+
+    fetchMovies();
+    fetchRecommendations();
+  }, [userID]);
 
   const goHome = () => {
     navigate("/");
@@ -67,42 +52,46 @@ export const Logueado = () => {
         {randomMovie && (
           <>
             <img
-              src={randomMovie.poster_path}
-              alt={randomMovie.title}
+              src={randomMovie.poster_link}
+              alt={randomMovie.series_title}
               className="featured-img"
             />
             <div className="movie-info">
-              <h1>{randomMovie.title}</h1>
+              <h1>{randomMovie.series_title}</h1>
             </div>
           </>
         )}
       </div>
 
       <div className="recommended">
-        <h2>Películas recomendadas</h2>
-        <div className="recommended-list">
-          {recommendedMovies.map((movie) => (
-            <div key={movie.id} className="movie-card">
-              <img
-                src={movie.poster_path}
-                alt={movie.title}
-              />
-              <h3>{movie.title}</h3>
-            </div>
-          ))}
+        <h2 className="letricablanca">Películas recomendadas</h2>
+        <div className="movie-list">
+          {recommendedMovies.length > 0 ? (
+            recommendedMovies.map((movie) => (
+              <div key={movie.id} className="movie-card">
+                <img
+                  src={movie.poster_link}
+                  alt={movie.series_title}
+                />
+                <h3>{movie.series_title}</h3>
+              </div>
+            ))
+          ) : (
+            <p>No se encontraron recomendaciones.</p>
+          )}
         </div>
       </div>
 
       <div className="all-movies">
-        <h2>Listado de películas</h2>
+        <h2 className="letricablanca">Listado de películas</h2>
         <div className="movie-list">
           {movies.map((movie) => (
             <div key={movie.id} className="movie-card">
               <img
-                src={movie.poster_path}
-                alt={movie.title}
+                src={movie.poster_link}
+                alt={movie.series_title}
               />
-              <h3>{movie.title}</h3>
+              <h3>{movie.series_title}</h3>
             </div>
           ))}
         </div>
